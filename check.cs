526f85ac015cbb3313cfd611306a1148ff8d0619
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-09-16 14:44:23 -0700

    Add files via upload

---
+++ check.cs
@@ -0,0 +1,9 @@
+namespace check
+{
+    class check1{
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("My name is priyam and C# is being difficult");
+        }
+    }
+}
