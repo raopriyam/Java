a90c0f466db12e4b40424eeb1e9e578bc45ccced
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-09-16 14:43:54 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        // static void main3(string[] args)
+        // {
+        //     Console.WriteLine("Priyam Rao");
+        // }
+    }
+}
